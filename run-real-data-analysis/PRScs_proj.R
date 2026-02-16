2d042370ed422bfdef2408bec0000bd2a661bb8c
Yuzheng Dun <ydun2@jhu.edu>
2026-02-15 20:37:23 -0500

    add PRS-CS-*

--- run-real-data-analysis/PRScs_proj.R
+++ run-real-data-analysis/PRScs_proj.R
@@ -9,49 +9,49 @@ percent_list = c(0.2, 0.4, 0.6, 0.8) # controlling percent of eigenvalue removed
 name_list = c('_auto', '_1', '_e2', '_e4', '_e6') # corresponding to the name of phi_list
 name_list_prs = c('auto', '1e+00', '1e-02', '1e-04', '1e-06') # corresponding to the name of output file of name_list
 run_sh_list = c()
-ref = '1kg' # can be 1kg or ukbb
-
-system(paste0("mkdir -p ", trait, "/", ref, "/PRScs_proj/run_sh"))
-system(paste0("mkdir -p ", trait, "/", ref, "/PRScs_proj/result"))
-for (chr in c(1:22)) {
-  for (phi_i in 1:length(phi_list)) {
-    for (percent_i in percent_list) {
-      for (a in a_list) {
-        phi = phi_list[phi_i]
-        name = name_list[phi_i]
-        run_sh = paste0( trait, '/', ref, '/PRScs_proj/run_sh/run_chr', chr, name, '_percent', percent_i, '_a', a, '.sh')
-        run_sh_list = c(run_sh_list, run_sh)
-        sink(run_sh)
-        cat('#!/bin/bash', "\n")
-        cat(paste0('mkdir -p ', trait,'/', ref, '/PRScs_proj/result/percent', percent_i), "\n")
-        sumdat = paste0(trait, '/data/chr', chr, '/PRScs_sumdat.txt')
-        ldsc = paste0(trait, '/data/chr', chr, '/ldsc-hm3.txt')
-        sumdat_tmp = bigreadr::fread2(ldsc)
-        N = as.integer(floor(mean(sumdat_tmp$N)))
-        output_dir = paste0(trait,'/', ref, '/PRScs_proj/result/percent', percent_i,'/chr', chr, '')
-        
-        sent_run <- paste0('python PRScs/PRScs_proj.py --ref_dir=', ref, '/ldblk_', ref, '_eur --bim_prefix=chr', chr,' --sst_file=', sumdat, ' --n_gwas=', N, ' --out_dir=',
-                           output_dir, ' --a=', a, ' --chrom=', chr, ' --phi=', phi, ' --eigenval_rm=', percent_i)
-        if(phi == 0){
-          sent_run <- paste0('python PRScs/PRScs_proj.py --ref_dir=', ref, '/ldblk_', ref, '_eur --bim_prefix=chr', chr,' --sst_file=', sumdat, ' --n_gwas=', N ,' --out_dir=',
-                             output_dir, ' --a=', a, ' --chrom=', chr, ' --eigenval_rm=', percent_i)
+for (ref in c("1kg", "ukbb")) {
+  
+  system(paste0("mkdir -p ", trait, "/", ref, "/PRScs_proj/run_sh"))
+  system(paste0("mkdir -p ", trait, "/", ref, "/PRScs_proj/result"))
+  for (chr in c(1:22)) {
+    for (phi_i in 1:length(phi_list)) {
+      for (percent_i in percent_list) {
+        for (a in a_list) {
+          phi = phi_list[phi_i]
+          name = name_list[phi_i]
+          run_sh = paste0( trait, '/', ref, '/PRScs_proj/run_sh/run_chr', chr, name, '_percent', percent_i, '_a', a, '.sh')
+          run_sh_list = c(run_sh_list, run_sh)
+          sink(run_sh)
+          cat('#!/bin/bash', "\n")
+          cat(paste0('mkdir -p ', trait,'/', ref, '/PRScs_proj/result/percent', percent_i), "\n")
+          sumdat = paste0(trait, '/data/chr', chr, '/PRScs_sumdat.txt')
+          ldsc = paste0(trait, '/data/chr', chr, '/ldsc-hm3.txt')
+          sumdat_tmp = bigreadr::fread2(ldsc)
+          N = as.integer(floor(mean(sumdat_tmp$N)))
+          output_dir = paste0(trait,'/', ref, '/PRScs_proj/result/percent', percent_i,'/chr', chr, '')
+          
+          sent_run <- paste0('python PRScs/PRScs_proj.py --ref_dir=data/', ref, '/ldblk_', ref, '_eur --bim_prefix=chr', chr,' --sst_file=', sumdat, ' --n_gwas=', N, ' --out_dir=',
+                             output_dir, ' --a=', a, ' --chrom=', chr, ' --phi=', phi, ' --eigenval_rm=', percent_i)
+          if(phi == 0){
+            sent_run <- paste0('python PRScs/PRScs_proj.py --ref_dir=data/', ref, '/ldblk_', ref, '_eur --bim_prefix=chr', chr,' --sst_file=', sumdat, ' --n_gwas=', N ,' --out_dir=',
+                               output_dir, ' --a=', a, ' --chrom=', chr, ' --eigenval_rm=', percent_i)
+          }
+          cat("module load anaconda \n")
+          cat(sent_run, "\n")
+          sink()
         }
-        cat("module load anaconda \n")
-        cat(sent_run, "\n")
-        sink()
       }
     }
   }
+  
+  run_all = paste0(-, "/", ref, "/PRScs_proj/run_sh/run_all.sh")
+  sink(run_all)
+  mem = 3
+  for (run_sh_i in 1:length(run_sh_list)) {
+    run_sh = run_sh_list[run_sh_i]
+    submit_sh = paste0('sbatch  --mem=', mem,'G ', run_sh)
+    cat(submit_sh, "\n")
+  }
+  sink()
+  system(paste0("bash ", run_all))
 }
-
-run_all = paste0(trait, "/", ref, "/PRScs_proj/run_sh/run_all.sh")
-sink(run_all)
-mem = 3
-for (run_sh_i in 1:length(run_sh_list)) {
-  run_sh = run_sh_list[run_sh_i]
-  submit_sh = paste0('sbatch  --mem=', mem,'G ', run_sh)
-  cat(submit_sh, "\n")
-}
-sink()
-system(paste0("bash ", run_all))
-
